fe6843dfa85e661996634f098c5cc86f21f5a542
Brian Pratama <brian.pratama91@gmail.com>
2021-01-28 11:25:05 +0700

    Undangan

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('#banner').backstretch([
+      "https://gyazo.com/a5f71135a95b759615d66b8d09886602.png"
+    , "https://gyazo.com/1a0145270bff5807bb3c2b213e94c604.png"
+    ], {duration: 3000, fade: 750});
+});
